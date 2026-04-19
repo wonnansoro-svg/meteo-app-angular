2023b7814ab6295f26ee541fbfdcb348c1be0d3d
wonnansoro-svg <wonnan.soro@uvci.edu.ci>
2026-04-19 04:53:09 +0200

    Lessage d'erreur

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,45 +1,50 @@
 import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // <-- 1. Importation indispensable pour la recherche
+import { FormsModule } from '@angular/forms';
 import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule], // <-- 2. Ajout dans les imports
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   protected readonly title = signal('meteo-app');
   weatherData = signal<any>(null); 
-  
-  // 3. Variable liée à notre barre de recherche
   cityToSearch = 'Dakar'; 
+  
+  // 1. Nouveau signal pour stocker le message d'erreur
+  errorMessage = signal<string | null>(null);
 
   private weatherService = inject(WeatherService);
 
   ngOnInit() {
-    // Au démarrage, on cherche la ville par défaut
     this.fetchWeather(this.cityToSearch);
   }
 
-  // 4. Fonction centralisée pour faire l'appel API
   fetchWeather(city: string) {
-    this.weatherData.set(null); // Optionnel: vide les données pour afficher le chargement
+    this.weatherData.set(null); 
+    this.errorMessage.set(null); // 2. On efface les anciennes erreurs à chaque recherche
+
     this.weatherService.getWeatherByCity(city).subscribe({
       next: (data: any) => {
         this.weatherData.set(data);
       },
       error: (err: any) => {
         console.error('Erreur API:', err);
-        // (La gestion propre de cette erreur sera le but du Sprint 3 !)
+        // 3. On vérifie le type d'erreur renvoyé par l'API
+        if (err.status === 404) {
+          this.errorMessage.set(`Oups ! La ville "${city}" est introuvable. Vérifiez l'orthographe.`);
+        } else {
+          this.errorMessage.set("Problème de réseau ou erreur de l'API. Veuillez réessayer plus tard.");
+        }
       }
     });
   }
 
-  // 5. Fonction déclenchée par le bouton "Rechercher"
   onSearch() {
-    if (this.cityToSearch.trim()) { // On vérifie que le champ n'est pas vide
+    if (this.cityToSearch.trim()) {
       this.fetchWeather(this.cityToSearch);
     }
   }
